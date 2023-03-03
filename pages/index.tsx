491471180f4c0f88952aa9eb7a048fd341aaaf67
Metehan Arıman <metehanariman94@gmail.com>
2023-03-04 01:15:25 +0300

    banner improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
-
+import Banner from '../components/Banner'
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* Banner */}
+      <Banner />
 
     </div>
   )
